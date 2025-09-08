0373a440d00d1e2d36171f4b2a6fa3c3035f246b
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-09-08 22:54:01 +0200

    Add relationship_app models and query samples

--- django-models/django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/django-models/LibraryProject/LibraryProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',  # <-- add this
+    'bookshelf',  
+    'relationship_app',
 ]
 
 
