fd8df10f6dd34fd11f5d968dcdc0f3fcbb5ea3eb
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-09-08 18:13:44 +0200

    Fix project structure and migrations for bookshelf app

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf'
+    'bookshelf',  # <-- add this
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
