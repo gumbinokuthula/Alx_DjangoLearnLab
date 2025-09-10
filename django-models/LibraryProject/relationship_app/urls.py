b1a52cc03707106e77bf8f28d3b713b1c526bb41
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-09-11 01:13:25 +0200

    Implement custom permissions for Book model and secure views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add-book/", views.add_book, name="add_book"),
-    path("edit-book/<int:book_id>/", views.edit_book, name="edit_book"),
-    path("delete-book/<int:book_id>/", views.delete_book, name="delete_book"),
+    # Add book
+    path('add_book/', views.add_book, name='add_book'),
+
+    # Edit book
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+
+    # Delete book
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
