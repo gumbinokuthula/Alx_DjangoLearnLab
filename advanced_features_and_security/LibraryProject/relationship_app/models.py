6836c165110586bd64d99c90ac3fff3bede76b7a
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-09-13 21:58:50 +0200

    added bookshelf

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,4 +1,17 @@
-from django.conf import settings
+from django.db import models
 
 class Book(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+    library = models.ForeignKey('Library', on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
