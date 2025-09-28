05eb39f5e199f72b2abf2105295b1d2fcff3ab09
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-09-28 18:11:09 +0200

    Task 3: Added unit tests for Book API including CRUD, permissions, and search

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,5 +1,33 @@
+# api/test_views.py
+
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 from .models import Author, Book
 from django.contrib.auth.models import User
+
+class BookAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password123")
+        self.author = Author.objects.create(name="J.K. Rowling")
+        self.book = Book.objects.create(title="Harry Potter", publication_year=1997, author=self.author)
+        self.client = self.client
+
+    def test_list_books(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_book_authenticated(self):
+        self.client.login(username='testuser', password='password123')
+        url = reverse('book-create')
+        data = {"title": "New Book", "publication_year": 2025, "author": self.author.id}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_book_unauthenticated(self):
+        url = reverse('book-create')
+        data = {"title": "Unauthorized Book", "publication_year": 2025, "author": self.author.id}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
